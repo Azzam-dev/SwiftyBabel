12a2f7546e4c6f1b59c52f8e60d062bff6e6f05e
Azzam R Alrashed <azzam.rar@gmail.com>
2023-08-10 15:26:21 +0300

    created swift Package

---
+++ Sources/SwiftyBabel/SwiftyBabel.swift
@@ -0,0 +1,6 @@
+public struct SwiftyBabel {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/SwiftyBabelTests/SwiftyBabelTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SwiftyBabel
+
+final class SwiftyBabelTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SwiftyBabel().text, "Hello, World!")
+    }
+}
